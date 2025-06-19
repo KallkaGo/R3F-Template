391f84ea11e58757e7d6e0e0c9d985720e24ffeb
KallkaGo <765830637@qq.com>
2025-06-19 11:34:26 +0800

    build: Delete things related to the sharing module

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { useEffect } from "react";
-import ShareMgr from "./mgr/ShareMgr";
 import ThreeContainer from "./three/ThreeContainer";
 import UIContainer from "./ui/UIContainer";
 import BrowserUtil from "@utils/Browser";
 
 export default function App() {
     useEffect(() => {
-        ShareMgr.shared.init();
         BrowserUtil.init();
     }, []);
 
